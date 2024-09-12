4b48bf68ff4060890c8119b3950a0fed3701f1a8
YueSu0225 <rechard0225@gmail.com>
2024-09-12 23:39:31 +0800

    20240912ecpay

---
+++ FinalS1/src/test/java/tw/Final/FinalS1/FinalS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.Final.FinalS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalS1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
