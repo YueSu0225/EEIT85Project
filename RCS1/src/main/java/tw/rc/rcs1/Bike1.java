7ccd4ab898694363b73ee95f1862b20c719b8f1d
rechard0225 <rechard0225@gmail.com>
2024-09-16 16:39:50 +0800

    20240916spring課程

---
+++ RCS1/src/main/java/tw/rc/rcs1/Bike1.java
@@ -0,0 +1,18 @@
+package tw.rc.rcs1;
+
+import org.springframework.stereotype.Component;
+
+
+public class Bike1 implements Bike{
+
+	@Override
+	public void upSpeed() {
+		System.out.println("Bike1:upSpeed");
+	}
+
+	@Override
+	public void downSpeed() {
+		System.out.println("Bike1:downSpeed");
+	}
+	
+}
