e1a7a1250db2170849463c59e4c4f895b2a73da1
Leo <badboyleooo@outlook.com>
2024-09-19 16:08:54 +0800

    整理

--- FinalS1/src/main/java/tw/Final/FinalS1/controller/FinalController.java
+++ FinalS1/src/main/java/tw/Final/FinalS1/controller/FinalController.java
@@ -1,4 +1,4 @@
-package tw.Final.FinalS1;
+package tw.Final.FinalS1.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import tw.Final.FinalS1.model.FinalMember;
+import tw.Final.FinalS1.repository.Finalface;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
